48abc1e8a50bb479e918cd5f1ea93bf5672675aa
07ashutoshkr <07ashutoshkr@gmail.com>
2020-11-05 20:25:13 +0530

    Trade Call Came on pagr from console

--- src/app/messages/messages.page.ts
+++ src/app/messages/messages.page.ts
@@ -1,90 +1,90 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
 
-import {Component, ViewChild, OnInit} from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
-import * as firebase from 'firebase';
-import { url } from 'inspector';
+import { Component, ViewChild, OnInit } from "@angular/core";
+import { AngularFireDatabase } from "@angular/fire/database";
+import { LoadingController } from '@ionic/angular';
+import * as firebase from "firebase";
+import { url } from "inspector";
+import { interval, Observable, Subscription } from "rxjs";
+
 
 // import {  } from 'rxjs';
-import {MessagePageServices } from "./message.page.service";
-
-
+import { MessagePageServices } from "./message.page.service";
 
 export interface Data {
-  
   movies: string;
 }
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.page.html',
-  styleUrls: ['./messages.page.scss'],
+  selector: "app-messages",
+  templateUrl: "./messages.page.html",
+  styleUrls: ["./messages.page.scss"],
 })
 export class MessagesPage implements OnInit {
-
   public data: Data;
   public columns: any;
   public rows: any;
-  public x:any;
-   url = 'https://cashcrow-c3ce7.firebaseio.com/Mastersheet/Telegram Calls/Calls';
-   constructor(private http:HttpClient,
-              private db: AngularFireDatabase,
-              private Msgpgsrvc :MessagePageServices)
-                 {
-                   {
-                       this.columns = [
-                                        { "name": 'Calls' }
-        
-                                      ];
-  
-                    //                      this.http.get<Data>("../../assets/movies.json")
-                    //                      .subscribe((res) => {
-                    //                       console.log(res)
-                    //                       this.rows = res.Calls;
-                    //                                 });
-                    // 
-                  }
- 
-                  }
-    ngOnInit(){
-      this.x= this.datafromFirebase();
+  public x: any;
+  url = "https://cashcrow-c3ce7.firebaseio.com/Mastersheet/";
+  sub: Subscription;
+  constructor(
+    private http: HttpClient,
+    private db: AngularFireDatabase,
+    private Msgpgsrvc: MessagePageServices,
+    public loadingctrl:LoadingController
+  ) {
+    {
+      var database = firebase.database();
+
+      var ref = database.ref("Mastersheet/Telegram Calls/Calls");
+      ref.on("value", (gotData) => {
+        this.data = gotData.val();
+        console.log(this.data);
+        return this.data;
+      });
+      const counter = interval(5000);
+      this.sub = counter.subscribe((val) => {
+        console.log("called");
+      });
+    }
+  }
+
+  ngOnInit() {
+    this.x = this.datafromFirebase();
     // this.Msgpgsrvc.gotdata().subscribe((res)=>{
     //   this.x = res
     //   console.log(this.x)
     // });
-      
-    }
-
-    datafromFirebase(){
-      // this.Msgpgsrvc.gotdata().subscribe(
-        
-      //   (res)=>{
-      //     const convdata = res 
-      //     console.log(convdata)});
-
-    
-      
-         var database = firebase.database();
-
-        var ref = database.refFromURL(this.url);
-       ref.on('value',gotData);
-      
-       function gotData(data)
-       {
-       var Data = data.val();
-       var keys = Object.keys(Data);
-      
-       console.log(Data);
-       return Data;
-      //  return Data ;
-
-       }
-    
- 
-   
-  
-    
-
   }
 
+  Refresh(){
+    this.loadingctrl.create({keyboardClose:true,message:"Refreshing.."})
+    .then((loadingR1)=>{
+      loadingR1.present();
+  this.x=this.datafromFirebase();
+  setTimeout(() => {
+    loadingR1.dismiss();
+  }, 2000);
+  
+})
+}
 
+  datafromFirebase() {
+    // this.Msgpgsrvc.gotdata().subscribe(
+
+    //   (res)=>{
+    //     const convdata = res
+    //     console.log(convdata)});
+
+    var database = firebase.database();
+    var ref = database.ref("Mastersheet/Telegram Calls/Calls");
+    ref.on("value", (gotData) => {
+      this.data = gotData.val();
+      console.log(this.data);
+      return this.data;
+    });
+
+    
+  }
+
+  
 }
